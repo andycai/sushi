3b1104b15b9ef134e9c87212b177e3ffbab0d3e2
Andy Cai <huayicai@gmail.com>
2026-04-21 19:43:14 +0800

    feat: update compiling style.css flow

--- scripts/watch-css.sh
+++ scripts/watch-css.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 监控 input.css 并自动编译为 style.css
-./temp/tailwindcss -i ./temp/input.css -o ./web/static/css/style.css --watch
+./temp/tailwindcss -i ./web/static/css/input.css -o ./web/static/css/style.css --minify --watch
